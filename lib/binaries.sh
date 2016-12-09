e74cf6438120f928b25ff1b79c56d33bedd16808
Alexey Noskov <alexey.noskov@gmail.com>
2016-12-09 17:27:12 +0300

    Allow to specify yarn download url in env

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -11,7 +11,7 @@ install_yarn() {
   local dir="$1"
 
   echo "Downloading and installing yarn..."
-  local download_url="https://yarnpkg.com/latest.tar.gz"
+  local download_url=${YARN_DOWNLOAD_URL:-https://yarnpkg.com/latest.tar.gz}
   local code=$(curl "$download_url" -L --silent --fail --retry 5 --retry-max-time 15 -o /tmp/yarn.tar.gz --write-out "%{http_code}")
   if [ "$code" != "200" ]; then
     echo "Unable to download yarn: $code" && false
